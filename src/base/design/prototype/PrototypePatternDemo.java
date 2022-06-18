099457d810c6aa5f17b309f47f30a3d1671cd47c
ktx <kangtianxing_1@163.com>
2022-06-18 21:22:27 +0800

    leetcode刷题仓库第一次提交

---
+++ src/base/design/prototype/PrototypePatternDemo.java
@@ -0,0 +1,20 @@
+package base.design.prototype;
+
+/**
+ * @author ktx
+ * @data 2022-06-18 21:15
+ */
+public class PrototypePatternDemo {
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+}
