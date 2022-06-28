1d46f0b5403ad8a5978dfcfea05311646f886da6
ktx <kangtianxing_1@163.com>
2022-06-28 09:50:30 +0800

    2022.06.27学习

--- src/nowcoder/dp/dp15Main.java
+++ src/nowcoder/dp/dp15Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
+ * Dilworth定理 最少的下降序列个数就等于整个序列最长上升子序列的长度
  * @author ktx
  * @data 2022-06-27 22:02
  */
@@ -19,25 +20,30 @@ public class dp15Main {
             arr[i] = scanner.nextInt();
         }
         int[] result = getResult(n,arr);
-        System.out.println(result);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
 
     }
 
     private static int[] getResult(int n, int[] arr) {
         int[] dp = new int[n];
+        int[] dp2 = new int[n];
         int[] res = new int[2];
         res[0] = 1;
-        res[1] = 1;
+        res[1] = 0;
         Arrays.fill(dp,1);
-        for (int i =1;i<n;i++){
-            for (int j = 0;j<i;j++){
-                if(arr[i]<arr[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+        Arrays.fill(dp2,1);
+        for (int i =1;i<n;i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] <= arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                } else {
+                    dp2[i] = Math.max(dp2[i], dp2[j] + 1);
                 }
             }
-            res[0] = Math.max(res[0],dp[i]);
+            res[0] = Math.max(res[0], dp[i]);
+            res[1] = Math.max(res[1], dp2[i]);
         }
-        List<Integer> list = new ArrayList<>();
         return res;
     }
 }
